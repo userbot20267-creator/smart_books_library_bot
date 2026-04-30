2deaed0c884aa65e451c187ae0864870e09c160d
userbot20267-creator <userbot20267@gmail.com>
2026-05-01 01:33:44 +0300

    Create favourite.py

---
+++ app/models/favourite.py
@@ -0,0 +1,16 @@
+"""Favorite Books Model"""
+
+from sqlalchemy import Column, Integer, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from app.database import Base
+
+class Favorite(Base):
+    __tablename__ = "favorites"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    book_id = Column(Integer, ForeignKey("books.id"), nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    user = relationship("User", backref="favorites")
+    book = relationship("Book", backref="favorites_book")
