a14c034f7a4a72d1b7ec62ea9201050613d52bdc
userbot20267-creator <userbot20267@gmail.com>
2026-04-30 14:57:18 +0300

    Update __init__.py

--- app/bot/__init__.py
+++ app/bot/__init__.py
@@ -1,19 +1,19 @@
 """Bot package"""
+from aiogram import Router  # ← أضف هذا السطر
 
 from .handlers import router as old_handlers_router
 from .handlers_features import router as new_features_router
 from .keyboards import get_main_keyboard, get_category_keyboard
 
-# دمج الراوترات: الجديد أولاً ليأخذ الأولوية
+# دمج الراوترات
 combined_router = Router()
 combined_router.include_router(new_features_router)
 combined_router.include_router(old_handlers_router)
+
+handlers_router = combined_router
 
 __all__ = [
     "handlers_router",
     "get_main_keyboard",
     "get_category_keyboard",
 ]
-
-# يُستخدم هذا المتغير في main.py
-handlers_router = combined_router
