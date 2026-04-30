dc2ad12ffcc7d113992c8faf9df03ff67a3c62c3
userbot20267-creator <userbot20267@gmail.com>
2026-05-01 01:34:20 +0300

    Create download_history.py

---
+++ app/models/download_history.py
@@ -0,0 +1,16 @@
+"""Download History Model"""
+
+from sqlalchemy import Column, Integer, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from app.database import Base
+
+class DownloadHistory(Base):
+    __tablename__ = "download_history"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    book_id = Column(Integer, ForeignKey("books.id"), nullable=False)
+    downloaded_at = Column(DateTime, default=datetime.utcnow)
+
+    user = relationship("User", backref="download_history")
+    book = relationship("Book", backref="download_history_book")
